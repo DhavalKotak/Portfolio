0f7c264c5a87def6f10713eea65c045024bc5392
DhavalKotak <kotakdhaval01@gmail.com>
2020-07-31 13:27:31 +0530

    deploy

---
+++ precache-manifest.207f1b1c784af4f4017785e70716a44c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91de16da2b4c97ee11b85c9e9d90941c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "42309ca4db2fca595499",
+    "url": "/portfolio/static/css/main.d39335ba.chunk.css"
+  },
+  {
+    "revision": "9e3e40f5d2dedb49518f",
+    "url": "/portfolio/static/js/2.59203b4e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.59203b4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42309ca4db2fca595499",
+    "url": "/portfolio/static/js/main.1a6b38ac.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "e92e8d0e7dc88b14193adbe0ce37ce5c",
+    "url": "/portfolio/static/media/bird.e92e8d0e.png"
+  },
+  {
+    "revision": "85a577f4eb1dbe9480cc8f539b29d97c",
+    "url": "/portfolio/static/media/icon.85a577f4.png"
+  },
+  {
+    "revision": "3122acc95a35a9f6435a1c671fa0df96",
+    "url": "/portfolio/static/media/reaction.3122acc9.png"
+  },
+  {
+    "revision": "492eb9585fc9d18db3cf008783e2ac29",
+    "url": "/portfolio/static/media/target.492eb958.png"
+  },
+  {
+    "revision": "5c044dfde6bbebfc481f1986f616e45a",
+    "url": "/portfolio/static/media/user.5c044dfd.png"
+  }
+]);
